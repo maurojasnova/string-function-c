354e9428e12f91c7e3740830b50f340d531acac8
maurojasnova <andrsnov@gmail.com>
2023-10-26 20:49:11 -0500

    First commit

---
+++ get-string.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+char *readDynamicString()
+{
+    size_t bufferSize = 128;                                       
+    char *inputString = (char *)malloc(bufferSize * sizeof(char)); 
+
+    if (inputString == NULL)
+    {
+        fprintf(stderr, "Memory allocation failed.\n");
+        return NULL;
+    }
+
+    size_t bytesRead = 0;
+    int ch;
+
+    //The characters entered by the user will be saved in an array until hit Enter.
+    while (1)
+    {
+        ch = getchar();
+
+        if (ch == '\n' || ch == EOF)
+        {
+            inputString[bytesRead] = '\0'; 
+            break;
+        }
+
+        inputString[bytesRead] = (char)ch;
+        bytesRead++;
+
+        //If the characters surpassed the initial buffersize, increase it by 128.
+        if (bytesRead >= bufferSize - 1)
+        {
+            bufferSize += 128; 
+            char *newString = (char *)realloc(inputString, bufferSize);
+
+            if (newString == NULL)
+            {
+                fprintf(stderr, "Memory reallocation failed.\n");
+                free(inputString);
+                return NULL;
+            }
+
+            inputString = newString;
+        }
+    }
+
+    return inputString;
+}
+
+int main()
+{
+    printf("Enter a string: ");
+    char *userInput = readDynamicString();
+
+    //Uncomment if you want to desallocate memory
+    // if (userInput != NULL)
+    // {
+    //     printf("You entered: %s\n", userInput);
+    //     free(userInput); 
+    // }
+
+    return 0;
+}
